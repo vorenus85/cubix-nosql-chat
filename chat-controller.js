220de9c6e7d94673e114edcedbb67343b4f1f6ec
Janos Perge <janos.perge@gmail.com>
2024-11-22 14:57:26 +0100

    add chat logic

--- chat-controller.js
+++ chat-controller.js
@@ -1,3 +1,41 @@
+// const _ = require('lodash')
+const chatService = require('./chat-service.js')
 const chatController = {}
 
+chatController.renderMessages = function (messages) {
+  const messagesPanel = document.getElementById('messages')
+  messagesPanel.innerHTML = ''
+  messages.forEach(message => {
+    messagesPanel.insertAdjacentHTML(
+      'beforeEnd',
+      `<div class="message-item mb-3"><p class="mb-1"><strong>${message.name}:</strong> ${message.message}</p><small><i>${message.datetime.toLocaleString()}</i></small></div>`
+    )
+  })
+}
+
+chatController.sendMessage = function () {
+  const loggedInUser = chatService.getLoggedInUser()
+  const messages = chatService.getMessages()
+  const messageInput = document.getElementById('messageInput')
+  if (!messageInput.value) {
+    return
+  }
+  const newMessage = {
+    ...loggedInUser,
+    message: messageInput.value,
+    datetime: new Date()
+  }
+  messages.push(newMessage)
+  chatController.renderMessages(messages)
+  messageInput.value = ''
+}
+
+chatController.logout = function () {
+  chatService.logout()
+}
+
+chatController.init = function () {
+  const messages = chatService.getMessages()
+  chatController.renderMessages(messages)
+}
 module.exports = chatController
--- chat-service.js
+++ chat-service.js
@@ -1,3 +1,45 @@
 const chatService = {}
 
+const loggedInUser = {
+  id: 1,
+  name: 'User 1'
+}
+
+let messages = [
+  {
+    id: 1,
+    name: 'User 1',
+    message: 'Hello',
+    datetime: new Date('2024-11-22 14:13:22')
+  },
+  {
+    id: 2,
+    name: 'User 2',
+    message: 'Hello there',
+    datetime: new Date('2024-11-22 14:15:22')
+  },
+  {
+    id: 3,
+    name: 'User 3',
+    message: 'I am here too!',
+    datetime: new Date('2024-11-22 14:18:22')
+  }
+]
+
+chatService.getLoggedInUser = function () {
+  return loggedInUser
+}
+
+chatService.getMessages = function () {
+  return messages
+}
+
+chatService.login = function () {}
+
+chatService.logout = function () {
+  alert('logout')
+}
+
+chatService.disconnect = function () {}
+
 module.exports = chatService
