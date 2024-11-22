6cfc5d381e5d79f397e3e35a7c02d7bf8a2baa92
Janos Perge <janos.perge@gmail.com>
2024-11-22 18:33:03 +0100

    working on rooms

--- chat-controller.js
+++ chat-controller.js
@@ -2,20 +2,63 @@
 const chatService = require('./chat-service.js')
 const chatController = {}
 
+let selectedRoom = chatService.defaultRoom()
+
 chatController.renderMessages = function (messages) {
   const messagesPanel = document.getElementById('messages')
   messagesPanel.innerHTML = ''
   messages.forEach(message => {
     messagesPanel.insertAdjacentHTML(
       'beforeEnd',
-      `<div class="message-item mb-3"><p class="mb-1"><strong>${message.name}:</strong> ${message.message}</p><small><i>${message.datetime.toLocaleString()}</i></small></div>`
+      `<div class="message-item mb-3 bg-light"><p class="mb-1"><strong>${message.name}:</strong> ${message.message}</p><small><i>${message.datetime.toLocaleString()}</i></small></div>`
     )
   })
 }
 
+chatController.renderRooms = function () {
+  const roomsPanel = document.getElementById('rooms-list')
+  roomsPanel.innerHTML = ''
+  const rooms = chatService.getRooms()
+  rooms.forEach(room => {
+    roomsPanel.insertAdjacentHTML(
+      'beforeend',
+      `<li class="list-group-item" onclick="chatController.selectRoom('${room.id}')">${room.name}</li>`
+    )
+  })
+}
+
+chatController.setSelectedRoom = function (room) {
+  selectedRoom = chatService.getRoom(room)
+}
+
+chatController.selectRoom = function (room) {
+  const messages = chatService.getMessages({ options: { room } })
+
+  chatController.setSelectedRoom(room)
+  chatController.renderSelectedRoomLabel()
+  chatController.renderMessages(messages)
+}
+
+chatController.renderSelectedRoomLabel = function () {
+  const roomLabel = document.getElementById('selectedRoom')
+  roomLabel.innerHTML = selectedRoom.name
+}
+
+chatController.addNewRoom = function () {
+  const roomInput = document.getElementById('roomInput')
+
+  if (!roomInput.value) {
+    return false
+  }
+
+  chatService.addNewRoom({ id: 'adasds', name: roomInput.value })
+  chatController.renderRooms()
+  roomInput.innerHTML = ''
+}
+
 chatController.sendMessage = function () {
   const loggedInUser = chatService.getLoggedInUser()
-  const messages = chatService.getMessages()
+  const messages = chatService.getMessages({ options: { room: selectedRoom.id } })
   const messageInput = document.getElementById('messageInput')
   if (!messageInput.value) {
     return
@@ -35,7 +78,7 @@ chatController.logout = function () {
 }
 
 chatController.init = function () {
-  const messages = chatService.getMessages()
-  chatController.renderMessages(messages)
+  chatController.selectRoom(selectedRoom.id)
+  chatController.renderRooms()
 }
 module.exports = chatController
--- chat-service.js
+++ chat-service.js
@@ -1,37 +1,64 @@
 const chatService = {}
 
+const defaultRoom = { id: 'room1', name: 'Room 1' }
+
 const loggedInUser = {
   id: 1,
   name: 'User 1'
 }
 
-let messages = [
-  {
-    id: 1,
-    name: 'User 1',
-    message: 'Hello',
-    datetime: new Date('2024-11-22 14:13:22')
-  },
-  {
-    id: 2,
-    name: 'User 2',
-    message: 'Hello there',
-    datetime: new Date('2024-11-22 14:15:22')
-  },
-  {
-    id: 3,
-    name: 'User 3',
-    message: 'I am here too!',
-    datetime: new Date('2024-11-22 14:18:22')
-  }
+let rooms = [
+  { id: 'room1', name: 'Room 1' },
+  { id: 'room2', name: 'Room 2' }
 ]
+
+let messages = {
+  room1: [
+    {
+      id: 1,
+      name: 'User 1',
+      message: 'Hello',
+      datetime: new Date('2024-11-22 14:13:22')
+    },
+    {
+      id: 2,
+      name: 'User 2',
+      message: 'Hello there',
+      datetime: new Date('2024-11-22 14:15:22')
+    },
+    {
+      id: 3,
+      name: 'User 3',
+      message: 'I am here too!',
+      datetime: new Date('2024-11-22 14:18:22')
+    }
+  ]
+}
+
+chatService.getRoom = function (roomId) {
+  return rooms.find(room => room.id === roomId)
+}
+
+chatService.defaultRoom = function () {
+  return defaultRoom
+}
 
 chatService.getLoggedInUser = function () {
   return loggedInUser
 }
 
-chatService.getMessages = function () {
-  return messages
+chatService.getMessages = function ({ options }) {
+  const { room } = options
+  return messages?.[room] || []
+}
+
+chatService.getRooms = function () {
+  return rooms
+}
+
+chatService.addNewRoom = function (room) {
+  console.log(room)
+  rooms.push(room)
 }
 
 chatService.login = function () {}
